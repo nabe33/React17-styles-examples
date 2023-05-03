3c2684dfba3804b2cc02e085b9b6468d4bf62052
Takayuki Watanabe <nabe@lab.twcu.ac.jp>
2023-05-03 14:15:21 +0900

    Initial commit

---
+++ src/components/CssModules.jsx
@@ -0,0 +1,10 @@
+import classes from "./CSSModules.module.scss";
+
+export const CssModules = () => {
+  return (
+    <div className={classes.container}>
+      <p className={classes.title}>- CSS Modules -</p>
+      <button className={classes.button}>Fight!</button>
+    </div>
+  );
+};
